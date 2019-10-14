999bd0d5afc74452771ad77b82e134f3013034d7
Wesley Szytka Gomes <wesley_szy@live.com>
2019-10-14 01:49:54 -0300

    Postman

--- src/main/java/com/szytka/szystem/resources/CategoriaResource.java
+++ src/main/java/com/szytka/szystem/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.szytka.szystem.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.szytka.szystem.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "HEHEHEHEHEHEH";
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista;
 		
 	}
 	
