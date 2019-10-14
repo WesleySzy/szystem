5f3c082e8bb86d96680ed10fba6269d77404183d
WesleySzy <wesley_szy@live.com>
2019-10-14 01:02:16 -0300

    Start project

---
+++ src/main/java/com/szytka/szystem/SzystemApplication.java
@@ -0,0 +1,13 @@
+package com.szytka.szystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szytka/szystem/SzystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.szytka.szystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SzystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
