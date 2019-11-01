d6abc335e415ac59321659c4cdd1ed4fff183e14
Wesley Szytka Gomes <wesley_szy@live.com>
2019-11-01 12:15:43 -0300

    Instancias no banco de dados

--- src/main/java/com/szytka/szystem/SzystemApplication.java
+++ src/main/java/com/szytka/szystem/SzystemApplication.java
@@ -1,13 +1,35 @@
 package com.szytka.szystem;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SzystemApplication {
+import com.szytka.szystem.domain.Categoria;
+import com.szytka.szystem.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SzystemApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SzystemApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 
 }
